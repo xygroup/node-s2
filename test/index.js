f7629933917d6bd0a0d656b9ee8cc39b30898d57
Erich Ocean <erich.ocean@me.com>
2016-05-30 01:57:08 -0700

    improved test code

--- test/index.js
+++ test/index.js
@@ -1,11 +1,11 @@
-var nativeExtension = require('../');
+var s2 = require('../');
 var assert = require('assert');
 
-describe('native extension', function() {
+describe('s2', function() {
   it("should export a function that calculates the distance between two points on the earth's surface", function() {
     var los_angeles = { lat: 34.0522, long: 118.2437 }
     var new_york = { lat: 40.7128, long: 74.0059 }
-    var dist = nativeExtension.DistanceBetweenLocations(los_angeles.lat, los_angeles.long, new_york.lat, new_york.long)
+    var dist = s2.DistanceBetweenLocations(los_angeles.lat, los_angeles.long, new_york.lat, new_york.long)
     var actual = 3940163 // floor of actual
     // console.log(dist)
     assert(typeof dist === 'number')
@@ -14,7 +14,7 @@ describe('native extension', function() {
 
   it("should throw when calling DistanceBetweenLocations with missing arguments", function() {
     try {
-      nativeExtension.DistanceBetweenLocations()
+      s2.DistanceBetweenLocations()
     } catch (e) {
       assert(true)
       return
@@ -23,7 +23,7 @@ describe('native extension', function() {
   })
 
   it("should export a function that calculates the level given a desired edge length in meters", function() {
-    var level = nativeExtension.GetClosestLevel(1000)
+    var level = s2.GetClosestLevel(1000)
     // console.log(level)
     assert(typeof level === 'number')
     assert(level === 13)
@@ -31,7 +31,7 @@ describe('native extension', function() {
 
   it("should throw when calling GetClosestLevel with missing arguments", function() {
     try {
-      nativeExtension.GetClosestLevel()
+      s2.GetClosestLevel()
     } catch (e) {
       assert(true)
       return
@@ -41,8 +41,8 @@ describe('native extension', function() {
 
   it("should export a function that calculates the cell at a given location and level", function() {
     var los_angeles = { lat: 34.0522, long: 118.2437 }
-    var level = nativeExtension.GetClosestLevel(1000)
-    var cell = nativeExtension.CellAtLocationAndLevel(los_angeles.lat, los_angeles.long, level)
+    var level = s2.GetClosestLevel(1000)
+    var cell = s2.CellAtLocationAndLevel(los_angeles.lat, los_angeles.long, level)
     // console.log(cell)
     assert(Buffer.isBuffer(cell))
     assert(cell.length === 8)
@@ -51,7 +51,7 @@ describe('native extension', function() {
 
   it("should throw when calling CellAtLocationAndLevel with missing arguments", function() {
     try {
-      nativeExtension.CellAtLocationAndLevel()
+      s2.CellAtLocationAndLevel()
     } catch (e) {
       assert(true)
       return
@@ -61,12 +61,12 @@ describe('native extension', function() {
 
   it("should export a function that calculates the cell ranges to search for a given lat/long, distance, and min/max level combination", function() {
     var los_angeles = { lat: 34.0522, long: 118.2437 }
-    var min_level = nativeExtension.GetClosestLevel(100000)
-    var max_level = nativeExtension.GetClosestLevel(1000)
+    var min_level = s2.GetClosestLevel(100000)
+    var max_level = s2.GetClosestLevel(1000)
     // console.log('min_level', min_level)
     // console.log('max_level', max_level)
     var distance = 5000
-    var ranges = nativeExtension.SearchRanges(los_angeles.lat, los_angeles.long, distance, min_level, max_level)
+    var ranges = s2.SearchRanges(los_angeles.lat, los_angeles.long, distance, min_level, max_level)
     // console.log(ranges.length)
     // console.log(ranges)
     assert(Array.isArray(ranges))
@@ -79,7 +79,7 @@ describe('native extension', function() {
 
   it("should throw when calling SearchRanges with missing arguments", function() {
     try {
-      nativeExtension.SearchRanges()
+      s2.SearchRanges()
     } catch (e) {
       assert(true)
       return
